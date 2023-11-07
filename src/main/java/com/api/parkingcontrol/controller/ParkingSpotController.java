ed7a3d74bfe3ce627882083bc168beb9b5d3528b
Jefferson Ferreira <emanuelly980280644@gmail.com>
2023-11-07 18:43:11 -0300

    att

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -1,9 +1,24 @@
 package com.api.parkingcontrol.controller;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 //essa anotação eu permito acessar de qualquer fonte
@@ -23,6 +38,29 @@ public class ParkingSpotController {
         this.parkingSpotService = parkingSpotService;
     }
 
+    @PostMapping
+    //aqui eu usei ResponseEntity para montar umas resposta tanto com status e também o corpo dessa
+    //resposta, e eu coloquei aqui object porque depois vamos ter diferentes tipo de retorno dependendo
+    //de algumas verificações que a gente vai fazer inicialmente então responseEntity Object, eu vou 
+    //receber o nosso Parkingspotdto que eu criei, aqui iremos receber como json, se eu não colocar @valid
+    //todas as validações que eu coloquei ParkingSpotDto não vão ser realizadas, assim se alguma validação
+    //estiver incorreta ele nem entra no método.
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        //posso fazer assim 
+        //ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+
+        //ou assim
+        var parkingSpotModel = new ParkingSpotModel();
+
+        //aqui eu vou converter o parkingSpotDto em parkingSpotModel
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+
+        //aqui eu estou setando a data de registro porque no parkingSpotDto não coloquei
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        //
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
     
 
 }
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -2,8 +2,10 @@ package com.api.parkingcontrol.services;
 
 
 import org.springframework.stereotype.Service;
-
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -19,5 +21,16 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    //quando é método construtivo ou destrutivo é interessante da gente anotar
+    //eles com @transactional principalmente quando a gente tem relacionamento
+    //né que vai ter deleção em Cascata o salvamento em Cascata é interessante
+    //a gente utilizar do @transitional por que se há algo de errado  durante
+    //essa translação ele garante que tudo volte ao normal certo e a gente não
+    //tenha dados quebrados vão dizer assim.
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
